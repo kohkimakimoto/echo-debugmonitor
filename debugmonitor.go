66bcdd6e964b405da7170345e75b794208e7da42
Kohki Makimoto <kohki.makimoto@gmail.com>
2025-09-07 22:44:52 +0900

    wip

--- debugmonitor.go
+++ debugmonitor.go
@@ -1,8 +1,18 @@
 package debugmonitor
 
+import (
+	"github.com/labstack/echo/v4"
+)
+
 type DebugMonitor struct {
 }
 
 func New() *DebugMonitor {
 	return &DebugMonitor{}
+}
+
+func Handler(m *DebugMonitor) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return nil
+	}
 }
