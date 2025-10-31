e2285da0d19e7501fd2347cc65a8436090d679e9
Kohki Makimoto <kohki.makimoto@gmail.com>
2025-10-31 10:40:28 +0900

    wip

--- resources/assets/app.js
+++ resources/assets/app.js
@@ -1,7 +1,9 @@
 import 'htmx.org';
 import Alpine from 'alpinejs';
 import NProgress from 'nprogress';
+import { showDangerToast } from "./lib/toasts";
 
+// extract javascript from SFC files
 import 'extract-sfc-script:../views/**/*.html';
 
 Alpine.start();
@@ -19,4 +21,12 @@ document.addEventListener('htmx:afterRequest', function (e) {
   // If there is a redirect, keep showing the progress bar until the redirect is complete
   if (e.detail.xhr.getResponseHeader('HX-Location') || e.detail.xhr.getResponseHeader('HX-Redirect')) return;
   NProgress.done();
+});
+
+// ----------------------------------------------------------------------------------------------
+// Handle network errors for htmx requests
+// ----------------------------------------------------------------------------------------------
+document.addEventListener('htmx:sendError', function (e) {
+  // Show a danger toast for network errors since there is no content to display from the server
+  showDangerToast('Network error occurred');
 });
