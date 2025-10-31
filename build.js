eec84b2f5f9af2cf6f6a564f98a39699eedba86c
Kohki Makimoto <kohki.makimoto@gmail.com>
2025-10-31 09:47:52 +0900

    up

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const esbuild = require('esbuild');
 const path = require('path');
 const fs = require('fs');
 const { execSync } = require('child_process');
-const { processCssExtraction, processScriptExtraction, processHtmlFile, cleanTempFiles } = require('./resources/extension/compile-sfc');
+const { processCssExtraction, processScriptExtraction, processHtmlFile, cleanTempFiles } = require('./resources/extension/sfc-builder');
 const { glob } = require('glob');
 const { minify } = require('html-minifier-next');
 
