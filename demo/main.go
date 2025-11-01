818430c0f8b344a68e3289aa802c7bb694b40b80
Kohki Makimoto <kohki.makimoto@gmail.com>
2025-11-02 07:17:09 +0900

    wip

--- demo/main.go
+++ demo/main.go
@@ -9,20 +9,20 @@ func main() {
 	e := echo.New()
 
 	dm := debugmonitor.New()
-	//monitor1 := &debugmonitor.Monitor{
-	//	Name:        "example_monitor1",
-	//	DisplayName: "Example Monitor1",
-	//	MaxRecords:  100,
-	//	Icon:        debugmonitor.IconExclamationCircle,
-	//}
-	//dm.AddMonitor(monitor1)
-	//monitor2 := &debugmonitor.Monitor{
-	//	Name:        "example_monitor2",
-	//	DisplayName: "Example Monitor2",
-	//	MaxRecords:  100,
-	//	Icon:        debugmonitor.IconCircleStack,
-	//}
-	//dm.AddMonitor(monitor2)
+	monitor1 := &debugmonitor.Monitor{
+		Name:        "example_monitor1",
+		DisplayName: "Example Monitor1",
+		MaxRecords:  100,
+		Icon:        debugmonitor.IconExclamationCircle,
+	}
+	dm.AddMonitor(monitor1)
+	monitor2 := &debugmonitor.Monitor{
+		Name:        "example_monitor2",
+		DisplayName: "Example Monitor2",
+		MaxRecords:  100,
+		Icon:        debugmonitor.IconCircleStack,
+	}
+	dm.AddMonitor(monitor2)
 	e.Any("/monitor", dm.Handler())
 
 	if err := e.Start(":8080"); err != nil {
